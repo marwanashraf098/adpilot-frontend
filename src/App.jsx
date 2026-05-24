911ee648c04aee32e5933e5dc86680463e8115b9
marwanashraf098 <marawan@Marawans-MacBook-Air.local>
2026-05-24 19:11:48 +0300

    Landing page only - all other routes hidden

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Dashboard from './pages/Dashboard'
-import CopyGenerator from './pages/CopyGenerator'
 import Landing from './pages/Landing'
 
 function App() {
-  const token = localStorage.getItem('token')
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/copy-generator" element={<CopyGenerator />} />
-        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   )
